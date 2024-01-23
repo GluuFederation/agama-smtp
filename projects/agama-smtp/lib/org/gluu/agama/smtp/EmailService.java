2cd3c4821ed99ceb2555beef62c9f678cb36bae3
Arnab Dutta <arnab.bdutta@gmail.com>
2024-01-23 11:48:39 +0530

    feat: add EmailService.java

---
+++ projects/agama-smtp/lib/org/gluu/agama/smtp/EmailService.java
@@ -0,0 +1,15 @@
+package org.gluu.agama.smtp;
+
+import org.gluu.agama.smtp.jans.service.JansEmailService;
+import org.gluu.agama.smtp.jans.model.ContextData;
+
+public abstract class EmailService {
+
+    public abstract Map<String, String> getUserMapUsingEmail(String email);
+    public abstract String sendEmail(String to, ContextData context);
+    public abstract String onboardUser(Map<String, String> profile, Set<String> attributes, String extUid) throws Exception;
+
+    public static EmailService getInstance(){
+        return  JansEmailService.getInstance();
+    }
+}
